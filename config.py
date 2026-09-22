1ab95a47328bd08fd3fdb695a16aec78f632dc88
Mattia <82594768+snaporaz812@users.noreply.github.com>
2026-09-22 03:38:14 +0200

    Removed config.py from caches files (it contains api keys)

--- config.py
+++
@@ -1,7 +0,0 @@
-HEADERS = {"User-Agent": "Mozilla/5.0"}
-
-ISBNs = [ #debug
-                "9780261102217", # "The Hobbit", English #debug
-                "9780679720201",  # "The Stranger", A. Camus, English #debug
-                "9788807904783" # "il Coccodrillo", F. Dostoevskij, Feltrinelli, Italiano, 2024 o 2026,
-            ]
